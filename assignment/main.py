1866d6f68bae2297f779364676fc2aae1019631e
shadowpr1est <67504491+shadowpr1est@users.noreply.github.com>
2024-11-15 11:05:37 +0500

    Update main.py

--- assignment/main.py
+++ assignment/main.py
@@ -51,5 +51,5 @@ def response():
     return render_template_string(html_template, greeting="Response Received!", question=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
